565c2295b38e4c88788123e2a0ae345508363416
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2025-04-02 18:13:24 +0530

    Add files via upload

--- src/API.ts
+++ src/API.ts
@@ -1,33 +1,27 @@
-import { shuffleArray } from './utils'
+import { shuffleArray } from './utils';
 
 export type Question = {
-    category: string;
-    correct_answer: string;
-    difficulty: string;
-    incorrect_answers: string[];
-    question: string;
-    type: string;
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
 };
 
-export type QuestionState = Question & { answers: string[] };
-
 export enum Difficulty {
-    EASY = "easy",
-    MEDIUM = "medium",
-    HARD = "hard"
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionState[]> => {
-    const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=18&type=multiple&difficulty=${difficulty}`;
-    const response = await fetch(endpoint);
-    const data = await response.json();
+export type QuestionsState = Question & { answers: string[] };
 
-    if (!data.results) {
-        throw new Error("No results found!");
-    }
-
-    return data.results.map((question: Question) => ({
-        ...question,
-        answers: shuffleArray([...question.incorrect_answers, question.correct_answer]) // Fixed 'answers' property
-    }));
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
 };
--- src/components/QCard.tsx
+++ src/components/QCard.tsx
@@ -1,33 +1,36 @@
-// Used Trivia api for 50 random questions on Computer Science: https://opentdb.com/api.php?amount=50&category=18&type=multiple
-import React from "react";
+import React from 'react';
 import { AnswerObject } from '../App';
-import { Wrapper, ButtonWrapper } from './QCard.styles'
+import { Wrapper, ButtonWrapper } from './QCard.styles';
 
 type Props = {
-    question: string;
-    answers: string[];
-    callback: (e: React.MouseEvent<HTMLButtonElement>)=>void;
-    userAnswer: AnswerObject | undefined;
-    questionNo: number;
-    totalQuestions: number;
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
 };
 
-const QCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNo, totalQuestions}) => (
-    <Wrapper>
-        <p className="number">
-            Question: {questionNo}/{totalQuestions}
-        </p>
-        <div dangerouslySetInnerHTML={{__html: question}}>
-            <div>
-                {answers.map(answer => (
-                    <ButtonWrapper key={answer} $correct={userAnswer?.correctAnswer===answer} $userClicked={userAnswer?.answer===answer}>
-                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
-                            <span dangerouslySetInnerHTML={{ __html: answer }} />
-                        </button>
-                    </ButtonWrapper>
-                ))}
-            </div>
-        </div>
-    </Wrapper>
+const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions}) => (
+  <Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
+    <div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          $correct={userAnswer?.correctAnswer === answer}
+          $userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
+    </div>
+  </Wrapper>
 );
-export default QCard
+
+export default QuestionCard;
